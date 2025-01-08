07fa8c0c141849c1deabee52470bc326ef8f342c
PrinOrange <prinorange@outlook.com>
2025-01-09 01:02:24 +0800

    fix: change package name.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Autodeployment
+# Autodeployee
 
 This is a simple node.js server that allows you to auto deploy github project on server with Github Webhook.
--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,5 +1,5 @@
-export const CONFIG_FILENAME = "autodeploy.config.json";
+export const CONFIG_FILENAME = "autodeployee.config.json";
 export const MAX_LOG_SIZE = 10485760;
 export const LOG_BACKUPS = 3;
-export const LOG_DIR = ".autodeploy";
-export const LOG_FILENAME = "autodeploy.log";
+export const LOG_DIR = ".autodeployee";
+export const LOG_FILENAME = "autodeployee.log";
